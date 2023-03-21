d84be3f9e00875182300ed0f387e3ec042f5f615
Karl Sirel <ksirel@tktk.ee>
2023-03-21 11:23:30 +0200

    add week7 code

---
+++ DiscountCalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿//total < 10 - 1%
+//total >= 10 && < 20 - 5%
+//total >= 20 - 10%
+
+Console.WriteLine("Provide total: ");
+int userInput = Int32.Parse(Console.ReadLine());
+
+double discount = CalculateDiscount(userInput);
+
+Console.WriteLine($"Discount is: {discount}%");
+
+double newTotal = CalculateNewTotal(userInput, discount);
+
+Console.WriteLine($"Uptated total with {discount}%: {newTotal}");
+
+
+
+static double CalculateDiscount(int total)
+{
+if(total < 10)
+    {
+        return 1;
+    }
+else if(total >= 10 && total < 20)
+    {
+        return 5;
+    }
+    else
+    {
+        return 10;
+    }
+}
+
+static double CalculateNewTotal(double total, double discount)
+{
+    double result = total - (total * discount) / 100;
+    return result;
+}
